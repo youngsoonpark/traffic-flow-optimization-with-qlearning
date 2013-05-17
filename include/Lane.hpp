033272582a998b8973acf8d78c0087958b2244be
ChrUbuntu User <user@ChrUbuntu.(none)>
2013-05-17 21:55:49 +1000

    Fleshed out the lane

--- include/Lane.hpp
+++ include/Lane.hpp
@@ -1,13 +1,21 @@
 #ifndef ROAD_CORE_LANE_H_
 #define ROAD_CORE_LANE_H_
 
+#include <deque>
+
 namespace road {
 namespace core {
 
 /**
  * POD struct holding data about the lane.
+ * We utilise a deque in this situation because each tick, at most 1 element is
+ * inserted to the head and 1 is deleted from the tail. We can preform these
+ * operations in constant time when utilising a deque.
  */
 struct Lane {
+	size_t max_capacity;   // Each lane can have a maximum capacity.
+	size_t speed_limit;    // Each lane can have a maximum speed limit.
+	std::deque<int> cells; // A lane consists of a series of cells.
 };
 
 } // End of namspace core.
