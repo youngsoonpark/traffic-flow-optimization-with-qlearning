a232c1b13f97053b8bdf6f1d7c897b6fc15c7173
Benjamin James Wright <bwright@cse.unsw.edu.au>
2013-05-24 22:49:49 +1000

    Moved serialization to XML

---
+++ source/SerializationPolicyXML.cpp
@@ -0,0 +1,14 @@
+#include "SerializationPolicyXML.hpp"
+#include "State.hpp"
+
+namespace road {
+namespace io {
+
+void SerializationPolicyXML::load(const std::string& filepath, core::State& state) const {
+}
+
+void SerializationPolicyXML::save(const core::State& state) const {
+}
+
+} // End of namespace core.
+} // End of namespace io.
