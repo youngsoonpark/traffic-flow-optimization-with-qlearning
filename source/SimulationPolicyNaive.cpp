8a57c9ef2eddc6303827d75277e923babefc79f2
Christopher <cjdibella@bigpond.com>
2013-06-08 15:48:05 +1000

    Comment out

--- source/SimulationPolicyNaive.cpp
+++ source/SimulationPolicyNaive.cpp
@@ -1,30 +1,53 @@
 #include "SimulationPolicyNaive.hpp"
+#include "State.hpp"
 #include <cstdlib>
 
 namespace road {
     namespace sim {
 
         void SimulationPolicyNaive::update(core::State& state) {
-            int chanceToGetCar = rand() % 100;
-            int road = rand() % 2;
-            State::Graph* g = state.getGraph();
+        /*    int chanceToGetCar = rand() % 101; // 101 so our range is [0, 100]
+            int road = rand() % 2; // Value meaning is dependent on State::Lights
+            core::State::Graph* g = state.getGraph();
 
+            core::Car tempCar;
+            std::string tempName;
+            size_t location;
             // move all cars up by 1
-            // TO DO: just committing for comments for now
-
-            // There is a 45% chance that a car will enter the lane
-            if (chanceToGetCar >= 45)
+            for (core::State::graph_traits::vertex_iterator vit = g->begin(); vit != g->end(); ++g)
             {
-                std::string edgeTrait = (road == Horisontal) ? "HorisontalSource" : "VerticalSource";
-
-                for (State::graph_traits::vertex_iterator vit = g->begin(); vit != g->end(); ++g)
+                location = vit->getTrait().find("Source");
+                if (location != std::string::npos)
                 {
-                    if (vit->getTrait() == edgeTrait && vit->somehowyouaccesstheedge.cars.size() <= state.maxCars())
+                    tempName = vit->getTrait().substr(0, location)
+                    tempCar = vit->somehowyouaccesstheedge.cars.front();
+                    vit->somehowyouaccesstheedge.cars.pop_front();
+
+                    if (tempName.find("Horisontal") != std::string::npos)
                     {
-                        vit->somehowyouaccesstheedge.cars.push(Car()vit->somehowyouaccesstheedge.speedlimit, 1.0));
+                        // somehow find the sink edge that joins intersection and sink
+                        // sink.push_back(tempCar)
+                    }
+                    else
+                    {
+                        // Do the same as above, but for vertical instead...
                     }
                 }
             }
+
+            // There is a 45% chance that a car will enter a lane
+            if (chanceToGetCar >= 45)
+            {
+                std::string edgeTrait = (road == State::Lights::Horisontal) ? "HorisontalSource" : "VerticalSource";
+
+                for (core::State::graph_traits::vertex_iterator vit = g->begin(); vit != g->end(); ++g)
+                {
+                    if (vit->getTrait() == edgeTrait && vit->somehowyouaccesstheedge.cars.size() <= state.maxCars())
+                    {
+                        vit->somehowyouaccesstheedge.cars.push_back(Car()vit->somehowyouaccesstheedge.speedlimit, 1.0));
+                    }
+                }
+            }*/
         }
     } // End of namespace sim.
 } // End of namespace road.
--- source/State.cpp
+++ source/State.cpp
@@ -1,12 +1,9 @@
 #include "State.hpp"
 
 namespace road {
-<<<<<<< HEAD
     namespace core {
 
         State::State(const int& maxCars) : m_running(true), m_lights(Lights::Horisontal), m_maxCars(maxCars) {
-            noCar.badboyFactor = 1.0;
-            noCar.speed = 1;
         }
 
         State::~State() {
@@ -39,48 +36,6 @@ namespace road {
             std::vector<int8_t>() x;
             return x;
         }*/
-
     } // End of namespace core.
-=======
-namespace core {
-
-State::State() : m_running(true) {
-}
-
-State::~State() {
-}
-
-bool State::isRunning() {
-    return m_running;
-}
-
-void State::setRunning(bool running) {
-    m_running = running;
-}
-
-State::Graph* State::getGraph() {
-    return &m_graph;
-}
-
-State::Lights State::getLights() const {
-    return m_lights;
-}
-
-void State::setLights(Lights lights) {
-    m_lights = lights;
-}
-
-int8_t State::feedback() const {
-    if (m_lights == Lights::Horisontal) {
-        // return the number of cars stopped in vertical edges * -1
-    } else if (m_lights == Lights::Vertical) {
-        // return the number of cars stopped in horisontal edges * -1
-    } else {
-        return 0;
-    }
-}
-
-} // End of namespace core.
->>>>>>> 2e33383836b177744c7e9f6d026b803a3527297a
 } // End of namespace road.
 
