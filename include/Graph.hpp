3d0c59f19768ea527742dd68de9e5d6cfab0beae
Benjamin James Wright <bwright@cse.unsw.edu.au>
2013-06-09 12:55:18 +1000

    The graph was fucking up our compile speed

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -99,199 +99,59 @@ public:
   /**
    * @description Constructs the default graph.
    */
-  Graph() {}
+  Graph();
 
   /**
    * @description returns a vertex, given its unique identifier.
    */
-  Vertex get_vertex(std::string uid) {
-    Vertex new_vertex;
-    new_vertex.type = m_graph[uid].type;
-    new_vertex.uid = m_graph[uid].uid;
-    new_vertex.x = m_graph[uid].x;
-    new_vertex.y = m_graph[uid].y;
-    return new_vertex;
-  }
+  Vertex get_vertex(std::string uid);
 
   /**
    * @description add a vertex, to the graph.
    */
-  void add_vertex(Vertex& to_add) {
-    boost::add_vertex(to_add.uid, m_graph);
-    // Add the properties.
-    m_graph[to_add.uid].type = to_add.type;
-    m_graph[to_add.uid].uid = to_add.uid;
-    m_graph[to_add.uid].x = to_add.x;
-    m_graph[to_add.uid].y = to_add.y;
-  }
+  void add_vertex(Vertex& to_add);
 
   /**
    * @description adds an edge with custom defined user data.
    */
-  void add_edge(std::string src, std::string dest, Edge& edge) {
-    edge_t new_edge;
-    bool b;
-    // Create the tie
-    boost::tie(new_edge, b) = boost::add_edge_by_label(src, dest, m_graph);
-    // Add the properties, to the graph
-    m_graph.graph()[new_edge].speed_limit = edge.speed_limit;
-    m_graph.graph()[new_edge].uid = edge.uid;
-    m_graph.graph()[new_edge].src = src;
-    m_graph.graph()[new_edge].dest = dest;
-    m_graph.graph()[new_edge].cars = edge.cars;
-    m_graph.graph()[new_edge].actual_cars = edge.actual_cars;
-  }
-
-  void add_edge(Vertex& src, Vertex& dest, Edge& edge) {
-    // Use the above, just retrieve the uid.
-    add_edge(src.uid, dest.uid, edge);
-  }
+  void add_edge(std::string src, std::string dest, Edge& edge);
+  void add_edge(Vertex& src, Vertex& dest, Edge& edge);
 
   /**
    * @description retrieves all the verticies
    */
-  std::list<Vertex> get_vertices() {
-    std::list<Vertex> vertices;
-    std::pair<vertex_iterator, vertex_iterator> it;
-
-    for (it = boost::vertices(m_graph); it.first != it.second; it.first++) {
-      // Retrieve the vertex
-      Vertex to_add;
-      // Add the properties to the vertex.
-      to_add.type = m_graph.graph()[*it.first].type;
-      to_add.x = m_graph.graph()[*it.first].x;
-      to_add.y = m_graph.graph()[*it.first].y;
-      to_add.uid = m_graph.graph()[*it.first].uid;
-      // Push the copy of the vertex.
-      vertices.push_back(to_add);
-    }
-    return vertices;
-  }
+  std::list<Vertex> get_vertices();
 
   /**
    * @description retrieves all the verticies of a given type.
    **/
-  std::list<Vertex> get_vertices(typename Vertex::Type type) {
-    // Define a nested predicate functor.
-    struct is_type {
-      typename Vertex::Type type;
-      is_type(typename Vertex::Type type) : type(type) {}
-      bool operator()(const Vertex& vertex) {
-        return vertex.type != type;
-      }
-    };
-    // Retrieve the list of verticies.
-    std::list<Vertex> vertices = get_vertices();
-    // Remove the ones we don't want.
-    vertices.remove_if(is_type(type));
-    // Return the list.
-    return vertices;
-  }
+  std::list<Vertex> get_vertices(typename Vertex::Type type);
 
   /**
    * @description retrieves all the edges in the graph.
    */
-  std::list<Edge> get_edges() {
-    std::list<Edge> edges;
-    std::pair<edge_iterator, edge_iterator> it;
-    // Iterate over the edges.
-    for (it = boost::edges(m_graph); it.first != it.second; it.first++) {
-      // Set the edge we want to add.
-      Edge to_add;
-      // Retrieve the source and destination.
-      vertex_t source = boost::source(*it.first, m_graph);
-      vertex_t destination = boost::target(*it.first, m_graph);
-      // Add properties to the edge.
-      // Add internal representation to the edge.
-      to_add.uid = m_graph.graph()[*it.first].uid;
-      to_add.speed_limit = m_graph.graph()[*it.first].speed_limit;
-      to_add.cars = m_graph.graph()[*it.first].cars;
-      to_add.actual_cars = m_graph.graph()[*it.first].actual_cars;
-      to_add.src = m_graph.graph()[source].uid;
-      to_add.dest = m_graph.graph()[destination].uid;
-      // Push the edge back.
-      edges.push_back(to_add);
-    }
-    // Returns the edges.
-    return edges;
-  }
+  std::list<Edge> get_edges();
 
   /**
    * @description get all the edges attached to a vertex, in both ways. This is pretty
    *  inefficient but for what we are simulating it ismore that sufficient.
    */
-  std::list<Edge> get_edges(Vertex& vertex) {
-    // Create the predicate functor, todo make efficent.
-    struct has_vertex {
-      Vertex& vertex;
-      has_vertex(Vertex& vertex) : vertex(vertex) {}
-      bool operator()(const Edge& edge) {
-        return edge.src != vertex.uid && edge.dest != vertex.uid;
-      }
-    };
-    // Retrieve the edges.
-    std::list<Edge> edges = get_edges();
-    // Remove the components of the edge we don't.
-    edges.remove_if(has_vertex(vertex));
-    // Rteturn the edge.
-    return edges;
-  }
+  std::list<Edge> get_edges(Vertex& vertex);
 
   /**
    * @description retrieves all the edges that come from the vertex.
    */
-  std::list<Edge> get_edges_from(Vertex& vertex) {
-    // Create the predicate functor, todo make efficient.
-    struct has_vertex {
-      Vertex& vertex;
-      has_vertex(Vertex& vertex) : vertex(vertex) {}
-      bool operator()(const Edge& edge) {
-        return edge.src != vertex.uid;
-      }
-    };
-    // Retrieve the edges.
-    std::list<Edge> edges = get_edges();
-    // Remove the components of the edge we don't.
-    edges.remove_if(has_vertex(vertex));
-    // Rteturn the edge.
-    return edges;
-  }
+  std::list<Edge> get_edges_from(Vertex& vertex);
 
   /**
    * @description retrievs all the edges that come to the vertex.
    */
-  std::list<Edge> get_edges_to(Vertex vertex) {
-    // Create the predicate functor, todo make efficient.
-    struct has_vertex {
-      Vertex& vertex;
-      has_vertex(Vertex& vertex) : vertex(vertex) {}
-      bool operator()(const Edge& edge) {
-        return edge.dest != vertex.uid;
-      }
-    };
-    // Retrieve the edges.
-    std::list<Edge> edges = get_edges();
-    // Remove the components of the edge we don't.
-    edges.remove_if(has_vertex(vertex));
-    // Rteturn the edge.
-    return edges;
-  }
+  std::list<Edge> get_edges_to(Vertex vertex);
 
   /**
    * @description provides an interface for updating an edge if it has been changed
    */
-  void update_edge(const Edge& edge) {
-    std::pair<edge_iterator, edge_iterator> it;
-    // Iterate over the edges.
-    for (it = boost::edges(m_graph); it.first != it.second; it.first++) {
-      if (m_graph.graph()[*it.first].uid == edge.uid) {
-        m_graph.graph()[*it.first].speed_limit = edge.speed_limit;
-        m_graph.graph()[*it.first].cars.assign(edge.cars.begin(), edge.cars.end());
-        m_graph.graph()[*it.first].actual_cars = edge.actual_cars;
-        break;
-      }
-    }
-  }
+  void update_edge(const Edge& edge);
 
 private:
   graph_t m_graph; // Graph member function.
--- source/SimulationPolicyNaive.cpp
+++ source/SimulationPolicyNaive.cpp
@@ -15,60 +15,61 @@ namespace sim
   // Debug message.
   std::cout << "SimulationPolicyNaive: Entering Simulaton" << std::endl;
   int chanceToGetCar = rand() % 101; // 101 so our range is [0, 100]
-  core::Graph* g = state.getGraph(); // Retrieve the graph.
+  core::Graph* graph = state.getGraph(); // Retrieve the graph.
 
-  core::Car tempCar(1, 1.0, false);    // This is a temporary location for our car
-  std::string tempName;                // This is the source's uid that makes it different from all other sources
+  core::Car tmp_car(1, 1.0, false);    // This is a temporary location for our car
+  std::string tmp_uid;                // This is the source's uid that makes source_it different from all other sources
   
-  auto verts_source = g->get_vertices(core::Vertex::Type::SOURCE);
-  auto verts_sinks = g->get_vertices(core::Vertex::Type::SINK);
-  auto intersection = g->get_vertices(core::Vertex::Type::INTERSECTION);
+  auto verts_source = graph->get_vertices(core::Vertex::Type::SOURCE);
+  auto verts_sinks = graph->get_vertices(core::Vertex::Type::SINK);
+  auto intersection = graph->get_vertices(core::Vertex::Type::INTERSECTION);
+
   // Edges from and to.
   core::Edge from, to;
 
   // move all cars up by 1
-  for (auto it = verts_source.begin(); it != verts_source.end(); it++) {
-    // Naive sources should only ever have one edge, so we can assume it is the back one
-    from = g->get_edges_from(*it).back();
-    // Get the only bit of source uid we can use to evaluate the sink
-    tempName = it->uid.substr(sizeof("source-"), std::string::npos);
+  for (auto source_it = verts_source.begin(); source_it != verts_source.end(); source_it++) {
+    // Naive sources should only ever have one edge, so we can assume source_it is the back one
+    from = graph->get_edges_from(*source_it).back();
+    // Get the only bsource_it of source uid we can use to evaluate the sink
+    tmp_uid = source_it->uid.substr(sizeof("source-"), std::string::npos);
     // If there are no cars, just continue.
     if (from.cars.empty()) {
       continue;
     }
-    // Get the car and then take it off the road
-    tempCar = from.cars.front();
+    // Get the car and then take source_it off the road
+    tmp_car = from.cars.front();
     from.cars.pop_front();
 
     // Decrement our counter each time we don't see a real car.
-    if (!tempCar.no_car) from.actual_cars--;
+    if (!tmp_car.no_car) from.actual_cars--;
     std::cout << "SimualtionPolicyNaive: Updating the road...." << std::endl;
-    // Re-add the edge to the graph (this is only a copy of it!)
-    g->update_edge(from);
+    // Re-add the edge to the graph (this is only a copy of source_it!)
+    graph->update_edge(from);
 
     // Now find the corresponding sink
-    for (auto jt = verts_sinks.begin(); jt != verts_sinks.end(); ++jt) {
+    for (auto sink_source_it = verts_sinks.begin(); sink_source_it != verts_sinks.end(); ++sink_source_it) {
       // We only care about the one that has the same uid as the source...
-      if (jt->uid.substr(sizeof("sink-"), std::string::npos) == tempName) {
-        // Again, Naive model, so we assume that there's only one edge leading to it
-        to = g->get_edges_to(*jt).back();
+      if (sink_source_it->uid.substr(sizeof("sink-"), std::string::npos) == tmp_uid) {
+        // Again, Naive model, so we assume that there's only one edge leading to source_it
+        to = graph->get_edges_to(*sink_source_it).back();
         // Take the front car away and add the car we just took from the source
         to.cars.pop_front();
-        to.cars.push_back(tempCar);
-        if (tempCar.no_car == false) to.actual_cars++;
-        // Re-add the edge to the graph (this is only a copy of it!)
-        g->update_edge(to);
-        // Remove this from the list since we don't need it anymore :)
-        //verts_sinks.remove(jt); <== obviously this isn't working atm; either going to find a different solution or just ignore altogether :(
+        to.cars.push_back(tmp_car);
+        if (tmp_car.no_car == false) to.actual_cars++;
+        // Re-add the edge to the graph (this is only a copy of source_it!)
+        graph->update_edge(to);
+        // Remove this from the list since we don't need source_it anymore :)
+        //verts_sinks.remove(sink_source_it); <== obviously this isn't working atm; esource_ither going to find a different solution or just ignore altogether :(
         break;
       }
     }
   }
 
   std::cout << "SimulationPolicyNaive: Adding cars to the " << verts_source.size()  << " road " << std::endl;
-  for (auto it = verts_source.begin(); it != verts_source.end(); ++it) {
+  for (auto source_it = verts_source.begin(); source_it != verts_source.end(); ++source_it) {
     // Retrieve the edge attached to the source.
-    from = g->get_edges_from(*it).back();
+    from = graph->get_edges_from(*source_it).back();
     // Randmly add a car to the state.
     if (chanceToGetCar < 45) {
       std::cout << " -> SimulationPolicyNaive: Car Not Added To " << from.uid << std::endl;
@@ -77,7 +78,7 @@ namespace sim
       std::cout << "-> SimulationPolicyNaive: Car Added Added To " << from.uid << std::endl;
       from.cars.push_back(core::Car(1, 1.0, true));
     }
-    g->update_edge(from);
+    graph->update_edge(from);
   }
   std::cout << "SimulationPolicyNaive: Ending Simulation" << std::endl;
 
